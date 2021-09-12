cd460f0282106c2d04977585d02fd5cf2a2f7fac
mbeniwal <mohit.beniwal@verizonmedia.com>
2021-09-12 20:18:40 +0530

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '1xtlh8qqf0'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-582dwhq4.us.auth0.com',
+  clientId: '6MjBxOWjJ5GUrEAOvgIe71qPXLQ5bE0A',
+  callbackUrl: 'http://localhost:3000/callback'
+}
